7dab38ec970a7af0e8f95b47d3ec603c9b48a4f3
Zach Edwards <zach.edwards@metadatatechnology.com>
2019-08-16 13:39:09 -0400

    added images

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,40 +2,40 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
-  host: "0.0.0.0",
-  titleTemplate: "%s - NWA",
-  siteDescription: "Creative technologist",
+  siteName: 'rotorink',
+  siteUrl: `https://www.rotorink-0.com`,
+  host: '0.0.0.0',
+  titleTemplate: '%s - NWA',
+  siteDescription: 'fantasy hockey news',
   plugins: [
     {
-      use: "@gridsome/source-filesystem",
+      use: '@gridsome/source-filesystem',
       options: {
-        path: "projects/**/*.md",
-        typeName: "ProjectPost",
+        path: 'projects/**/*.md',
+        typeName: 'ProjectPost',
         resolveAbsolutePaths: true,
         remark: {
-          externalLinksTarget: "_blank",
-          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+          externalLinksTarget: '_blank',
+          externalLinksRel: ['nofollow', 'noopener', 'noreferrer']
         }
       }
     },
     {
-      use: "@gridsome/source-filesystem",
+      use: '@gridsome/source-filesystem',
       options: {
-        path: "journal/**/*.md",
-        typeName: "JournalPost",
+        path: 'journal/**/*.md',
+        typeName: 'JournalPost',
         resolveAbsolutePaths: true,
         remark: {
-          externalLinksTarget: "_blank",
-          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+          externalLinksTarget: '_blank',
+          externalLinksRel: ['nofollow', 'noopener', 'noreferrer']
         }
       }
     }
   ],
   transformers: {
     remark: {
-      plugins: ["@gridsome/remark-prismjs"]
+      plugins: ['@gridsome/remark-prismjs']
     }
   }
 };
