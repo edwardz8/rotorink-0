e76d3b5abb23a8eadd9934f1b69d4be5b199f5ec
Zach Edwards <zach.edwards@metadatatechnology.com>
2019-08-22 21:20:09 -0400

    job posting'

---
+++ src/pages/Jobs.vue
@@ -0,0 +1,48 @@
+<template>
+  <Layout>
+    <div class="container">
+      <div class="contact-header">
+        <h1 class="contact-title">Jobs</h1>
+
+        <p>
+          RotoRink is looking for talented and driven Fantasy Hockey Writers to craft content for our digital platform.
+          The ideal candidate will possess good writing and research skills and take pride in their work.
+          The applicant will be a team player, a fan of the game of hockey and be able to give and receive constructive feedback.
+          You should be a stickler for meeting deadlines and have the willingness to stay up-to-date with breaking hockey news.
+        </p>
+
+        <h2>Featured Writers</h2>
+        <hr />
+        <p>
+          We are looking for talented and driven NHL Fantasy Hockey Writers to craft featured length articles for our digital platform.
+          The main goal of this role is to create insightful content revolving around the NHL through engaging and stat-driven prose.
+        </p>
+
+        <h2>Player Update Writers</h2>
+        <hr />
+        <p>Player Update Writers will craft breaking player news. You should be a stickler for meeting deadlines and have the willingness to stay up-to-date with breaking hockey news.</p>
+
+        <p>Send all inquiries, explaining why you feel you would be the perfect fit, to: rotorinkhq@gmail.com</p>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.contact-header {
+  padding: 2rem 0 4rem 0;
+}
+.contact-title {
+  font-size: 4rem;
+  margin: 0 0 4rem 0;
+  padding: 0;
+}
+p {
+  margin-bottom: 3rem;
+}
+</style>
+
